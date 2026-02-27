976bd3e80a1c2ba21c5ff421132973be281d39a5
alireza-khosroabadi <khosroabadi.alireza@gmail.com>
2026-02-28 00:25:15 +0330

    add dependencies

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
     implementation(project(":core"))
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.javax.inject)
 
     testImplementation(libs.junit)
 }
