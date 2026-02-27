ff4fbf371fee638cb4605e4521eb2e752a617a2f
alireza-khosroabadi <khosroabadi.alireza@gmail.com>
2026-02-27 23:59:56 +0330

    add AppResult and ConnectionStatus models

--- core/src/main/java/com/mbank/price/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.mbank.price
-
-class MyClass {
-}
---
+++ core/src/main/java/com/mbank/price/model/connectionStatus/ConnectionStatus.kt
@@ -0,0 +1,7 @@
+package com.mbank.price.model.connectionStatus
+
+enum class ConnectionStatus {
+    CONNECTING,
+    CONNECTED,
+    DISCONNECTED
+}
