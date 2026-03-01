9b00d73c855bbd7893a3cd76ad8dcf9be4d1567a
alireza-khosroabadi <khosroabadi.alireza@gmail.com>
2026-03-01 17:08:56 +0330

    create Data layer datamodels, update domain models

--- domain/src/main/java/com/mbank/price/domain/model/price/StockPrice.kt
+++ domain/src/main/java/com/mbank/price/domain/model/price/StockPrice.kt
@@ -2,11 +2,6 @@ package com.mbank.price.domain.model.price
 
 data class StockPrice(
     val currentPrice: Double,
-    val previousPrice: Double
-){
-    val priceStatus: PriceStatus = when{
-        currentPrice > previousPrice -> PriceStatus.UP
-        currentPrice < previousPrice -> PriceStatus.DOWN
-        else -> PriceStatus.NO_CHANGE
-    }
-}
+    val priceStatus: PriceStatus,
+    val lastChangeAtMillis: Long
+)
