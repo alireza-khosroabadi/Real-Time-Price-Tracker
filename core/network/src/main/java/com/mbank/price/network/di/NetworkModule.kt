408fb1672b2917853dec1e3090c32a87a14f72cf
alireza-khosroabadi <khosroabadi.alireza@gmail.com>
2026-03-02 16:45:29 +0330

    add network module di

--- core/network/src/main/java/com/mbank/price/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/mbank/price/network/di/NetworkModule.kt
@@ -1,4 +1,38 @@
 package com.mbank.price.network.di
 
-class NetworkModule {
+import com.mbank.price.common.model.annotation.ApplicationScope
+import com.mbank.price.network.webSocket.OkHttpWebSocketFactory
+import com.mbank.price.network.webSocket.WebSocketFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import okhttp3.OkHttpClient
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideWebSocketFactory(
+        factory: OkHttpWebSocketFactory
+    ): WebSocketFactory = factory
+
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope =
+        CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+
 }
