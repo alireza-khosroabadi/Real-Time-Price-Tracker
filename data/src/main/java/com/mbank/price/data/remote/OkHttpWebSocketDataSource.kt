0310772ac5ebb1624ef71ffcc6bbc3f3152520e6
alireza-khosroabadi <khosroabadi.alireza@gmail.com>
2026-03-03 17:25:02 +0330

    remove commented codes

--- data/src/main/java/com/mbank/price/data/remote/OkHttpWebSocketDataSource.kt
+++ data/src/main/java/com/mbank/price/data/remote/OkHttpWebSocketDataSource.kt
@@ -51,14 +51,12 @@ class OkHttpWebSocketDataSource @Inject constructor(private val webSocketFactory
 
     override fun start(
         symbols: List<String>,
-//        seedPrices: Map<String, BigDecimal>
     ) {
         scope.launch {
             mutex.withLock {
                 trackedSymbols.clear()
                 trackedSymbols.addAll(symbols)
                 currentPrices.clear()
-//                currentPrices.putAll(seedPrices)
             }
         }
         _isRunning.update { true }
